b3be11c9f215fc967ae52ea199213f35b6089868
t-Asai <takumi.a42@gmail.com>
2018-10-27 19:36:31 +0900

    refactor

--- test.py
+++ test.py
@@ -9,39 +9,19 @@ import torchvision.utils as vutils
 from network.Transformer import Transformer
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_dir', default='test_img')
-parser.add_argument('--load_size', default=450)
-parser.add_argument('--model_path', default='./pretrained_model')
-parser.add_argument('--style', default='Hayao')
-parser.add_argument('--output_dir', default='test_output')
-parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument("--input_dir", default="test_img")
+parser.add_argument("--load_size", default=450)
+parser.add_argument("--model_path", default="./pretrained_model")
+parser.add_argument("--style", default="Hayao")
+parser.add_argument("--output_dir", default="test_output")
+parser.add_argument("--gpu", type=int, default=0)
 
 opt = parser.parse_args()
 
-valid_ext = ['.jpg', '.png']
+valid_ext = [".jpg", ".png"]
 
-if not os.path.exists(opt.output_dir):
-    os.mkdir(opt.output_dir)
 
-# load pretrained model
-model = Transformer()
-model.load_state_dict(torch.load(os.path.join(
-    opt.model_path, opt.style + '_net_G_float.pth')))
-model.eval()
-
-if opt.gpu > -1:
-    print('GPU mode')
-    model.cuda()
-else:
-    print('CPU mode')
-    model.float()
-
-for files in os.listdir(opt.input_dir):
-    ext = os.path.splitext(files)[1]
-    if ext not in valid_ext:
-        continue
-    # load image
-    input_image = Image.open(os.path.join(opt.input_dir, files)).convert("RGB")
+def convert_image(model, input_image):
     # resize image, keep aspect ratio
     h = input_image.size[0]
     w = input_image.size[1]
@@ -71,8 +51,46 @@ for files in os.listdir(opt.input_dir):
     output_image = output_image[[2, 1, 0], :, :]
     # deprocess, (0, 1)
     output_image = output_image.data.cpu().float() * 0.5 + 0.5
-    # save
-    vutils.save_image(output_image, os.path.join(
-        opt.output_dir, files[:-4] + '_' + opt.style + '.jpg'))
+    return output_image
 
-print('Done!')
+
+def main():
+    if not os.path.exists(opt.output_dir):
+        os.mkdir(opt.output_dir)
+
+    # load pretrained model
+    model = Transformer()
+    model.load_state_dict(torch.load("{dir}/{name}".format(**{
+        "dir": opt.model_path,
+        "name": "{}_net_G_float.pth".format(opt.style)
+    })))
+    model.eval()
+
+    if opt.gpu > -1:
+        print("GPU mode")
+        model.cuda()
+    else:
+        print("CPU mode")
+        model.float()
+
+    for files in os.listdir(opt.input_dir):
+        ext = os.path.splitext(files)[1]
+        if ext not in valid_ext:
+            continue
+        print(files)
+        # load image
+        input_image = Image.open(os.path.join(
+            opt.input_dir, files)).convert("RGB")
+        output_image = convert_image(model, input_image)
+        # save
+        vutils.save_image(output_image, "{dir}/{name}{ext}".format(**{
+            "dir": opt.output_dir,
+            "name": "{}_{}".format(files[:-4], opt.style),
+            "ext": ext
+        }))
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
